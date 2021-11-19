5f9be0178715b89d82fe883b93b055df75d40dcc
Moshe Borochov <info@lamemebete.xyz>
2021-11-18 19:22:58 -0500

    WP

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ import time
 from scipy.interpolate import interp1d
 import socket
 import random
+import copy
 UDP_IP = "127.0.0.1"
 UDP_PORT = 6400
 MESSAGE = ""
-attempts = 10
+attempts = 20
 # Internet socket.SOCK_DGRAM) # UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-time_to_eat = 1
+time_to_eat = 1000
 population = ['g', 's', 'b']
 g_r = 0.4
-max_nodes = 5
+max_nodes = 10
 sim_min_range = -150
 sim_max_range = 150
 x_1 = 1
 y_1 = 1
-max_readius_slime_connection = 60
+max_readius_slime_connection = 100
 
 
 def LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-100):
     ndotu = planeNormal.dot(rayDirection)
     if abs(ndotu) < epsilon:
-        print('enters')
         raise RuntimeError("no intersection or line is within plane")
 
     w = rayPoint - planePoint
@@ -39,18 +39,29 @@ def find_close_node(node, nodes):
         if np.linalg.norm(cand['cord'] - node['cord']) < max_readius_slime_connection:
             selection.append(i)
     if len(selection) > 0:
-        return selection[random.randint(0, len(selection)-1)]
+        return nodes[selection[random.randint(0, len(selection)-1)]]
     return False
 
-def plot_values(nodes, t, color, final_csv):
-    for node in nodes:
+def plot_values(nodes, t, color, final_csv, recent_node):
+    '''for node in nodes:
         node_2 = find_close_node(node, nodes)
         node_3 = find_close_node(node, nodes)
         if node_2:
             final_csv += str(node['cord'][0]) + ',' + str(node['cord'][1]) + ',' + str(node['cord'][2]) + ',' + str(nodes[node_2]['cord'][0]) + ',' + str(nodes[node_2]['cord'][1]) + ',' + str(nodes[node_2]['cord'][2]) + ',' + str(t) + ',' + 'line' +  ',' + color + '\n'
         if node_3:
             final_csv += str(node['cord'][0]) + ',' + str(node['cord'][1]) + ',' + str(node['cord'][2]) + ',' + str(nodes[node_3]['cord'][0]) + ',' + str(nodes[node_3]['cord'][1]) + ',' + str(nodes[node_3]['cord'][2]) + ',' + str(t) + ',' + 'line' +  ',' + color + '\n'
-    return final_csv
+    '''
+    if recent_node == False:
+        node = random.choice(nodes)
+    else:
+        node = recent_node
+    closest_node = find_close_node(node, nodes)
+    if closest_node == False:
+        return final_csv, recent_node
+    else:
+        node_2 = closest_node
+        final_csv += str(node['cord'][0]) + ',' + str(node['cord'][1]) + ',' + str(node['cord'][2]) + ',' + str(node_2['cord'][0]) + ',' + str(node_2['cord'][1]) + ',' + str(node_2['cord'][2]) + ',' + str(t) + ',' + 'line' +  ',' + color + '\n'
+        return final_csv, node_2
 
 def plot_food(food, t, color, final_food_csv):
     for f in food:
@@ -102,6 +113,19 @@ def init_points(n, init_r, factor):
         nodes.append({'cord': random_point, 'state': 'g'})
     return nodes
 
+def rescale_points(values):
+    values_to_return = []
+    for i, dim in enumerate(values):
+        values_to_return.append([])
+        r_max = max(dim)
+        r_min = min(dim)
+        t_max = (r_max - r_min)/2
+        t_min = -(r_max - r_min)/2
+        for m in dim:
+            values_to_return[i].append(((m - r_min)*(t_max-t_min)/(r_max-r_min)) + t_min)
+            #values_to_return[i].append(m - r_min)
+    return values_to_return
+
 
 def init_food(n, min_bound, max_bound, r_range):
     food = []
@@ -113,6 +137,21 @@ def init_food(n, min_bound, max_bound, r_range):
         #z = 0
         food.append({'cord': np.array([x, y, z]),
                     'count': 0, 'r': r, 'type': True})
+    return food
+
+def generate_food_from_file(filename, r_range):
+    values = [[],[],[]]
+    food = []
+    with open(filename) as file:
+        for line in file:
+            tmp_food = line.rstrip().split(",")
+            tmp_food = [float(i) for i in tmp_food]
+            for i, val in enumerate(tmp_food):
+                values[i].append(val)
+    food_scaled = rescale_points(values)
+    for i in range(len(food_scaled[0])):
+        tmp_food = [food_scaled[0][i], food_scaled[1][i], food_scaled[2][i]]
+        food.append({'cord': np.array([80*tmp_food[0], 80*tmp_food[1], 80*tmp_food[2]]), 'count': 0, 'r': r_range*random.random()+3, 'type': True})
     return food
 
 
@@ -188,7 +227,7 @@ def generate_one_cycle(nodes,food, p_pos, p_neg, p_neu, negative_food):
             while check_if_intersect(negative_food, new_direction):
                 n_f = find_intersect_food(negative_food, new_direction)
                 if n_f != False:
-                    print('expansion grow')
+                    #print('expansion grow')
                     # Define plane
                     norm_n_f = -n_f['cord'] + node['cord']
                     planeNormal = norm_n_f
@@ -213,7 +252,7 @@ def generate_one_cycle(nodes,food, p_pos, p_neg, p_neu, negative_food):
             while check_if_intersect(negative_food, cord_1):
                 n_f = find_intersect_food(negative_food, cord_1)
                 if n_f != False:
-                    print('branch grow')
+                    #print('branch grow')
                     cord_1 = node['cord'] + g_r * (-node['cord'] + closest_food['cord'])
                     norm_n_f = -n_f['cord'] + node['cord']
                     # Define plane
@@ -271,15 +310,23 @@ if __name__ == "__main__":
     p_pos = [0.3, 0.1, 0.6]
     p_neu = [0.3, 0.1, 0.6]
     nodes = [init_points(5, 1, -110), init_points(5, 1, 80)]
-    #nodes = [init_points(5, 1, -110), []]
+    #nodes = [init_points(10, 1, -110), []]
 
     food_1 = init_food(250, sim_min_range, sim_max_range, 5)
     food_2 = init_food(150, sim_min_range, sim_max_range, 5)
-    #food = generate_grid_food(2, -5, 5, 10)
+    food = generate_grid_food(2, -5, 5, 10)
 
-    #food_2 = [{'cord': np.array([0, 0, 0]), 'r': 150, 'type': True}, {'cord': np.array([100, 100, 100]), 'r': 40, 'type': True}]
+    food_3 = generate_food_from_file("pt_radiation.txt", 5)
+    food_4 = generate_food_from_file("pt_stress.txt", 5)
+    #food_4 = subtract_intersecting_food(food_3, food_4)
+
+    food_2 = [{'cord': np.array([0, 0, 0]), 'r': 80, 'type': True}, {'cord': np.array([100, 100, 100]), 'r': 1, 'type': True}]
     food_1 = subtract_intersecting_food(food_2, food_1)
-    food = [food_1, food_2]
+
+
+
+    #food = [food_1, food_2]
+    food = [food_3, food_4]
     color = ['r', 'b']
     final_nodes = ""
     food_csv_string = ""
@@ -287,13 +334,16 @@ if __name__ == "__main__":
     nodes_2_csv_String = ""
 
     generation = 0
+    recent_node = [False, False]
     while True:
         empty_food = False
         nodes, food = generate_cycles(nodes, food, p_pos, p_neg, p_neu)
-        nodes_string = ""
         food_string = ""
+        nodes_string = ""
         for i, nodes_set in enumerate(nodes):
-            nodes_string += plot_values(nodes_set, generation, color[i], "")
+            tmp_nodes_string, tmp = plot_values(nodes_set, generation, color[i], "", recent_node[i])
+            recent_node[i] = copy.deepcopy(tmp)
+            nodes_string += tmp_nodes_string
         for i, food_set in enumerate(food):
             food_string += plot_food(food_set,generation, color[i], "")
         food_csv_string += food_string
@@ -312,3 +362,4 @@ if __name__ == "__main__":
     text_file = open("food_res.txt", "w")
     n = text_file.write(food_csv_string)
     text_file.close()
+    sock.sendto(str.encode('d'), (UDP_IP, UDP_PORT))
